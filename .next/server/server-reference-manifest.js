25b891f6bd59b27d25254925a185a3ba470aceef
sanberkzulfikar <tosanberk@icloud.com>
2024-11-16 06:32:07 +0300

    import fix6

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"az15NSoOqce918THTQf1jxOMJHNdRXri1lsVonv7rWQ=\"\n}"
---
+++ .next/static/chunks/app_dashboard_layout_tsx_4cd700._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_dashboard_layout_tsx_4cd700._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_dashboard_layout_tsx_4cd700._.js",
+  "chunks": [
+    "static/chunks/node_modules_21b78f._.js",
+    "static/chunks/app_b0570c._.js"
+  ],
+  "source": "dynamic"
+});
